5217acbeb82b7138efd1d12faee58a4237a83a1d
vinitkumarphp <vinit1988php@gmail.com>
2021-07-12 21:44:11 -0700

    First Commit

---
+++ resources/js/app.js
@@ -0,0 +1,18 @@
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
+require('./bootstrap');
+
+import {createApp} from 'vue';
+
+import ExampleComponent from './components/ExampleComponent.vue';
+
+createApp({
+
+    components:{
+        ExampleComponent,
+    }
+}).mount('#app')
